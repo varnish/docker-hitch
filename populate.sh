57fa845473eb58cf54605137cd395bd7cdeb2678
Guillaume Quintard <guillaume@varnish-software.com>
2020-09-04 14:13:23 -0700

    fix the maintainers section

--- populate.sh
+++ populate.sh
@@ -6,8 +6,8 @@ other_tags="1, 1.6, 1.6.1"
 populate_library() {
 	cat > library.hitch <<- EOF
 # this file was generated using https://github.com/varnish/docker-hitch/blob/`git rev-parse HEAD`/populate.sh
-Maintainers: Thijs Feryn <thijs@varni.sh> (@thijsferyn)
-             Guillaume Quintard (@gquintard)
+Maintainers: Thijs Feryn <thijs@varni.sh> (@thijsferyn),
+             Guillaume Quintard <guillaume@varni.sh> (@gquintard)
 GitRepo: https://github.com/varnish/docker-hitch.git	
 
 Tags: $other_tags, $latest_version latest
