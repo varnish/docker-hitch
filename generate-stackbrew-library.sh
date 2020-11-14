f2228347d214aac9f03c05bf3c4061b72d14458a
Guillaume Quintard <guillaume@varnish-software.com>
2020-11-13 22:41:52 -0800

    add github actions

---
+++ generate-stackbrew-library.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cat > library.varnish <<- EOF
+# this file was generated using https://github.com/varnish/docker-hitch/blob/`git rev-parse HEAD`/generate-stackbrew-library.sh
+Maintainers: Thijs Feryn <thijs@varni.sh> (@thijsferyn),
+             Guillaume Quintard <guillaume@varni.sh> (@gquintard)
+GitRepo: https://github.com/varnish/docker-hitch.git
+
+Tags: 1, 1.6, 1.6.1, 1.6.1-1, latest
+Architectures: amd64
+GitCommit: `git rev-parse HEAD`
+EOF
